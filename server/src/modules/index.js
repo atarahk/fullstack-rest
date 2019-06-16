ff4deeb7b3c54fdbb7b216b51c1c7fd8e28f4f6b
Atarah <atarahplayground@gmail.com>
2019-06-17 07:37:39 +0800

    server update

--- server/src/modules/index.js
+++ server/src/modules/index.js
@@ -1,9 +1,11 @@
 const todoRouter = require('./todos')
+const userRouter = require('./user')
 
 const apiPrefix = '/api/'
 
 const routes = app => {
   app.use(apiPrefix, todoRouter)
+  app.use(apiPrefix, userRouter)
   return app
 }
 
