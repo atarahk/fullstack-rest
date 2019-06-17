3d8fba989a76c8acdbe2b2792b8bb7211269209b
Atarah <atarahplayground@gmail.com>
2019-06-17 21:22:43 +0800

    up

--- server/src__/bin/www.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-const app = require('../app')
-
-const port = parseInt(process.env.PORT, 10) || 5000
-const server = http.createServer(app)
-server.listen(port, (req, res) => {
-  console.log(`App is running on: ${port}`)
-})
